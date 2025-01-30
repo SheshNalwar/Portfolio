db0b81a9dfdc4ff18b64b9b3c1b28f372e9c295b
Umesh Nagare <umeshnagare27@gmail.com>
2025-01-30 18:44:34 +0530

    SVG Logos added

--- src/app/Components/skillSection/SkillData.jsx
+++ src/app/Components/skillSection/SkillData.jsx
@@ -1,56 +1,71 @@
 import {
-    AppwriteIcon,
-    CIcon,
-    ExpressJSIcon,
-    FirebaseIcon,
-    GitHubIcon,
-    GSAPIcon,
-    HTMLIcon,
-    JavaIcon,
-    JavaScriptIcon,
-    MongoDBIcon,
-    MySql,
-    NodeJSIcon,
-    ReactJSIcon
+  AppwriteIcon,
+  CIcon,
+  ExpressJSIcon,
+  FirebaseIcon,
+  GitHubIcon,
+  GSAPIcon,
+  HTMLIcon,
+  JavaIcon,
+  JavaScriptIcon,
+  MongoDBIcon,
+  MySql,
+  NodeJSIcon,
+  ReactJSIcon,
+  TailwindCSSIcon,
 } from "../Icons";
 
 export const skillsData = [
-    {
-        name: "React Js",
-        icon: <ReactJSIcon />
-
-    }, {
-        name: "Express Js",
-        icon: <ExpressJSIcon />
-    }, {
-        name: "Node Js",
-        icon: <NodeJSIcon />
-    }, {
-        name: "Mongo DB",
-        icon: <MongoDBIcon />
-    }, {
-        name: "Java",
-        icon: <JavaIcon />
-    }, {
-        name: "MySQL",
-        icon: <MySql />
-    }, {
-        name: "Firebase",
-        icon: <FirebaseIcon />
-    }, {
-        name: "Appwrite",
-        icon: <AppwriteIcon />
-    }, {
-        name: "HTML",
-        icon: <HTMLIcon />
-    }, {
-        name: "JavaScript",
-        icon: <JavaScriptIcon />
-    }, {
-        name: "GSAP",
-        icon: <GSAPIcon />
-    }, {
-        name: "GitHub",
-        icon: <GitHubIcon />
-    }
-]
+  {
+    name: "React Js",
+    icon: <ReactJSIcon />,
+  },
+  {
+    name: "Express Js",
+    icon: <ExpressJSIcon />,
+  },
+  {
+    name: "Node Js",
+    icon: <NodeJSIcon />,
+  },
+  {
+    name: "Mongo DB",
+    icon: <MongoDBIcon />,
+  },
+  {
+    name: "Java",
+    icon: <JavaIcon />,
+  },
+  {
+    name: "MySQL",
+    icon: <MySql />,
+  },
+  {
+    name: "Firebase",
+    icon: <FirebaseIcon />,
+  },
+  {
+    name: "Appwrite",
+    icon: <AppwriteIcon />,
+  },
+  {
+    name: "HTML",
+    icon: <HTMLIcon />,
+  },
+  {
+    name: "Tailwind CSS",
+    icon: <TailwindCSSIcon />,
+  },
+  {
+    name: "JavaScript",
+    icon: <JavaScriptIcon />,
+  },
+  {
+    name: "GSAP",
+    icon: <GSAPIcon />,
+  },
+  {
+    name: "GitHub",
+    icon: <GitHubIcon />,
+  },
+];
